4d1c62cb7b935f1a0ab3fc50b37689879f5f9ee3
Jean-Matthieu DECHRISTE <jeanmatthieu@dechriste.fr>
2018-06-10 13:11:26 +0200

    Bug fixes

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,33 @@
 #define DISTANCE_THRESHOLD_PERCENT 0.90
 #define THRESHOLD_MAX_DURATION 2000
 #define TIME_BETWEEN_READS 200
+#define TIME_BETWEEN_SPRAY 10000
 #define PIN_SWITCH 8
+
+#define PIN_STEPPER_INPUT_1 2
+#define PIN_STEPPER_INPUT_2 3
+#define PIN_STEPPER_INPUT_3 4
+#define PIN_STEPPER_INPUT_4 5
+#define PIN_ULTRASONIC_ECHO 6
+#define PIN_ULTRASONIC_TRIG 7
 
 // The gear reduction ratio is approximately 64:1 (tested: 63.68395:1)
 // 64 steps per motor rotation x 63.684 gear ratio = 4076 steps per full revolution (approximately)
 const int stepsPerRevolution = 4076;
 
-Stepper stepper(stepsPerRevolution, 2, 3, 4, 5);
-Ultrasonic ultrasonic(6, 7);
+Stepper stepper(stepsPerRevolution,
+  PIN_STEPPER_INPUT_1, PIN_STEPPER_INPUT_2,
+  PIN_STEPPER_INPUT_3, PIN_STEPPER_INPUT_4);
+Ultrasonic ultrasonic(PIN_ULTRASONIC_ECHO, PIN_ULTRASONIC_TRIG);
 
 unsigned int refMeasure = 0;
 unsigned long thresholdReachedTimestamp = 0;
 unsigned long lastReadTimestamp = 0;
+unsigned long lastSprayTimestamp = 0;
 bool sprayActive = false;
 bool checkSwitch = false;
+
+void powerOffStepper();
 
 void setup() {
   Serial.begin(115200);
@@ -35,11 +48,13 @@ void setup() {
   unsigned int distanceSum = 0;
   while(nbMeasures < REF_MEASURE_COUNT) {
     delay(TIME_BETWEEN_READS);
+    digitalWrite(LED_BUILTIN, HIGH);
     unsigned int distance = ultrasonic.distanceRead();
     if(distance != 0) {
       distanceSum += ultrasonic.distanceRead();
       nbMeasures++;
     }
+    digitalWrite(LED_BUILTIN, LOW);
   }
   refMeasure = (int)round(distanceSum / REF_MEASURE_COUNT);
   Serial.print("Ref distance value: ");
@@ -49,7 +64,7 @@ void setup() {
 void loop() {
   if(!sprayActive) {
     // - Let's avoid any echo from the previous reading
-    if(millis() - lastReadTimestamp > TIME_BETWEEN_READS) {
+    if(millis() - lastReadTimestamp > TIME_BETWEEN_READS && millis() - lastSprayTimestamp > TIME_BETWEEN_SPRAY) {
       lastReadTimestamp = millis();
 
       // - If the distance is less than 90%, the threshold is reached
@@ -81,10 +96,19 @@ void loop() {
     if(!checkSwitch && digitalRead(PIN_SWITCH) == HIGH) {
       checkSwitch = true;
     } else if(checkSwitch && digitalRead(PIN_SWITCH) == LOW) {
+      lastSprayTimestamp = millis();
       checkSwitch = false;
       sprayActive = false;
       thresholdReachedTimestamp = 0;
+      powerOffStepper();
       Serial.println("Spray inactive");
     }
   }
+}
+
+void powerOffStepper() {
+  digitalWrite(PIN_STEPPER_INPUT_1, LOW);
+  digitalWrite(PIN_STEPPER_INPUT_2, LOW);
+  digitalWrite(PIN_STEPPER_INPUT_3, LOW);
+  digitalWrite(PIN_STEPPER_INPUT_4, LOW);
 }
